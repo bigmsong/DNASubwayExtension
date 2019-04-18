d510a55a32811d069e2a44cb8aec96c80769355e
Paul <32556413+bigmsong@users.noreply.github.com>
2019-04-18 00:22:17 -0400

    Fixed type/iframe issues

--- DNA/clicks.js
+++ DNA/clicks.js
@@ -1,7 +1,8 @@
 var clickList = new Array();
 var numOfClicks = 0;
-var cliekdType = '';
+var clickedType = '';
 var word = '';
+var clickedElement = '';
 
 chrome.storage.local.get('num', function(result){
 	if(result.num != null) {
@@ -22,35 +23,87 @@ function clickOrigin(e){
 }
 
 document.onclick = clickListener;
-document.onkeypress = keyListener;
 
-function keyListener(e) {
-	var charType = e.key;
-	if(clickList[clickList.length-1] == (' name') || clickList[clickList.length-1] == (' description') || clickList[clickList.length-1] == (' profile-query')) {
-		word += String(e.key);
-	}
-}
 
 function clickListener(e) {
-    var clickedElement;
+	var addClick;
+	if(clickList.length > 10) {
+		clickList = new Array();
+	}
     clickedType = clickOrigin(e).tagType;
     numOfClicks++;
     if(clickList.length > 0) {
-    	if(clickList[clickList.length-1] == (' name') || clickList[clickList.length-1] == (' description') || clickList[clickList.length-1] == (' profile-query') && word != '') {
-    		clickList.push('Typed: ' + word);
-    		word = '';
+    	if(clickList[clickList.length-1] == (' name')) {
+    		clickList.push(' Typed: ' + document.getElementById('name').value);
+    	}
+    	if(clickList[clickList.length-1] == (' description')) {
+    		clickList.push(' Typed: ' + document.getElementById('description').value);
+    	}
+    	if(clickList[clickList.length-1] == (' ftitle')) {
+    		clickList.push(' Typed: ' + document.getElementById('ftitle').value);
+    	}
+    	if(clickList[clickList.length-1] == (' forg')) {
+    		clickList.push(' Typed: ' + document.getElementById('forg').value);
+    	}
+    	if(clickList[clickList.length-1] == (' fowner')) {
+    		clickList.push(' Typed: ' + document.getElementById('fowner').value);
+    	}
+    	if(clickList[clickList.length-1] == (' quality_threshold')) {
+    		clickList.push(' Typed: ' + document.getElementById('quality_threshold').value);
+    	}
+    	if(clickList[clickList.length-1] == (' min_length')) {
+    		clickList.push(' Typed: ' + document.getElementById('min_length').value);
+    	}
+    	if(clickList[clickList.length-1] == (' min_quality')) {
+    		clickList.push(' Typed: ' + document.getElementById('min_quality').value);
+    	}
+    	if(clickList[clickList.length-1] == (' percent_bases')) {
+    		clickList.push(' Typed: ' + document.getElementById('percent_bases').value);
+    	}
+    	if(clickList[clickList.length-1].includes(': sample')) {
+    		var temp = rowNum();
+    		clickList.push(' Typed: ' + document.getElementsByClassName('sample')[temp].value);
+    	}
+    	if(clickList[clickList.length-1].includes(': condition')) {
+    		var temp= rowNum();
+    		clickList.push(' Typed: ' + document.getElementsByClassName('condition')[temp].value);
     	}
 	}
+	alert(clickedType);
     if(clickedType != 'a' && clickedType != 'input' && clickedType != 'b' && clickedType != 'option' && clickedType != 'textarea' && clickedType != 'label' && clickedType != 'button') {
-    	clickedElement = 'misclick';
+    	addClick = 'misclick';
+    	if(clickedType == 'select') {
+    		addClick = 'selecting';
+    	}
+    	if(clickedType == 'img') {
+    		if(e.target.id == "kallisto-tip") {
+    			addClick = "Help Button";
+    		}
+    		if(e.target.title == 'View Output in CyVerse Data Store') {
+    			addClick = "Discover Data Store";
+    		}
+    	}
     	if(String(window.location.pathname).includes('/about/resources.html') && clickedType == 'span') {
     		typeOfElement(e.target.parentNode);
     	}
     }
     else {
-    	clickedElement = typeOfElement(e);
+    	if(clickedType == 'input') {
+    		addClick = e.target.id;
+    		if(e.target.id == '') {
+    			addClick = e.target.className;
+    			if(addClick == 'sample' || addClick == 'condition') {
+    				addClick = e.target.parentNode.parentNode.cells[0].innerText + " and " 
+    				+ e.target.parentNode.parentNode.cells[1].innerText + ": " 
+    				+ e.target.className + " - " + e.target.parentNode.parentNode.sectionRowIndex;
+    			}
+    		}
+    	}
+    	else {
+    		addClick = typeOfElement(e);
+		}
 	}
-	clickList.push(' ' + clickedElement);
+	clickList.push(' ' + addClick);
 	//comment out next line for stopping the annoying alerts
 	alert(clickList);
 	chrome.storage.local.set({ 'num': numOfClicks }, function(){
@@ -59,11 +112,48 @@ function clickListener(e) {
 	});
 }
 
+function rowNum() {
+	var string = "";
+	string = clickList[clickList.length-1];
+	if (string.includes('0')) {
+		return 0;
+	}
+	if (string.includes('1')) {
+		return 1;
+	}
+	if (string.includes('2')) {
+		return 2;
+	}
+	if (string.includes('3')) {
+		return 3;
+	}
+	if (string.includes('4')) {
+		return 4;
+	}
+	if (string.includes('5')) {
+		return 5;
+	}
+	if (string.includes('6')) {
+		return 6;
+	}
+	if (string.includes('7')) {
+		return 7;
+	}
+	if (string.includes('8')) {
+		return 8;
+	}
+	if (string.includes('9')) {
+		return 9;
+	}
+	return null;
+}
+
 function typeOfElement(e) {
 
    	if(clickedType == 'b') {
    		clickedElement = 'Log Out';
    	}
+
 
    	else if(e.target == 'https://cyverse-dnasubway-guide.readthedocs-hosted.com/en/latest/step7.html') {
    		clickedElement = 'Green Line Tutorial';
@@ -138,7 +228,7 @@ function typeOfElement(e) {
    	}
 
    	else if(e == 'http://blast.ncbi.nlm.nih.gov/Blast.cgi') {
-   		clickElement = 'BLAST';
+   		clickedElement = 'BLAST';
    	}
 
    	else if(e == 'http://www.iplantcollaborative.org/') {
@@ -331,8 +421,27 @@ function typeOfElement(e) {
     }
 
     else {
-    	clickedElement = e.target.innerText;
+    	if(e.target.innerText == "Download" && clickedType == 'button') {
+    		clickedElement = "Downloading: " + e.target.parentNode.parentNode.parentNode.cells[0].innerText;
+    	}
+    	else {
+    		clickedElement = e.target.innerText;
+    	}
 	}
+
+	if(e.target.parentNode != null) {
+   		if(e.target.parentNode.parentNode != null) {
+   			if(e.target.parentNode.parentNode.parentNode != null) {
+   				if(e.target.parentNode.parentNode.parentNode.parentNode != null) {
+   				   	if(e.target.parentNode.parentNode.parentNode.parentNode.parentNode.id != null) {
+   						if(e.target.parentNode.parentNode.parentNode.parentNode.parentNode.id == 'manage_data') {
+   							clickedElement = "Pariing: " + e.target.parentNode.parentNode.parentNode.cells[1].innerText;
+   						}
+   					}		
+   				}
+   			}
+   		}
+   	}
 
     return clickedElement;
 }
